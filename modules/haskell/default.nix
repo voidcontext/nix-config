4ecf2d4dbe3369971849e5096118e1a67cd0576f
Gabor Pihaj <gabor.pihaj@gmail.com>
2020-01-04 13:20:41 +0000

    Add packages to help switching from stack to nix

--- modules/haskell/default.nix
+++ modules/haskell/default.nix
@@ -5,6 +5,8 @@ let
 in
 {
   home.packages = [
+    pkgs.cabal-install
+    pkgs.cabal2nix
     # Install stable HIE for GHC 8.6.5
     (all-hies.selection { selector = p: { inherit (p) ghc865; }; })
   ];
