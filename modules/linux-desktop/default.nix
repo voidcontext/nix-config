96c1de8f6da46f84b90b9e995a122e11c2e1971e
Gabor Pihaj <gabor.pihaj@gmail.com>
2020-01-09 22:17:10 +0000

    Use Earthsong terminal color theme

--- modules/linux-desktop/default.nix
+++ modules/linux-desktop/default.nix
@@ -4,9 +4,9 @@ with builtins;
 
 let
   xtermTheme = fetchurl {
-    name = "BelafonteNight";
-    url = "https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/master/Xresources/Belafonte%20Night";
-    sha256 = "12yf3a45xmi69422crxh0pk2cvc982jmg2lxks274ryl978yxxjs";
+    name = "Earthsong";
+    url = "https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/master/Xresources/Earthsong";
+    sha256 = "1hvhqn6qrylyxmyz1icap1l2mk3741xv7b6lqymjjn4sxw5dd244";
   };
 in
 {
@@ -20,9 +20,9 @@ in
 
   home.file.".xmobar/xmobarrc".source =  ./xmobarrc;
   home.file.".xmonad/xmonad.hs".source = ./xmonad.hs;
-  home.file.".themes/BelafonteNight.Xresources".source = xtermTheme;
+  home.file.".themes/Earthsong.Xresources".source = xtermTheme;
   home.file.".Xresources".text = ''
-  #include $HOME/.themes/BelafonteNight.Xresources
+  #include $HOME/.themes/Earthsong.Xresources
 
   *xterm*faceName: "Monaco for Powerline:antialias=true"
   *xterm*faceSize: 10
@@ -34,7 +34,6 @@ in
   Xft.hintstyle: hintslight
   Xft.rgba: rgb
   Xft.lcdfilter: lcddefault
-
   '';
 
   programs.gnome-terminal = {
@@ -43,31 +42,31 @@ in
     profile = {
       "4eeb888c-f65f-432c-8ece-881255dacb3f" = {
         default = true;
-        visibleName = "Belafonte Night - Nix";
+        visibleName = "Earthsong nix";
         cursorShape = "block";
         font = "Monaco 10";
         showScrollbar = false;
         colors = {
-          foregroundColor = "#96968c8c8383";
-          backgroundColor = "#202011111b1b";
-          boldColor = "#96968c8c8383";
+          foregroundColor = "#e5e5c7c7a9a9";
+          backgroundColor = "#292925252020";
+          boldColor = "#e5e5c7c7a9a9";
           palette = [
-            "#202011111b1b"
-            "#bebe10100e0e"
-            "#858581816262"
-            "#eaeaa5a54949"
-            "#42426a6a7979"
-            "#979752522c2c"
-            "#98989a9a9c9c"
-            "#96968c8c8383"
-            "#5e5e52525252"
-            "#bebe10100e0e"
-            "#858581816262"
-            "#eaeaa5a54949"
-            "#42426a6a7979"
-            "#979752522c2c"
-            "#98989a9a9c9c"
-            "#d5d5ccccbaba"
+            "#121214141818"
+            "#c9c942423434"
+            "#8585c5c54c4c"
+            "#f5f5aeae2e2e"
+            "#13139898b9b9"
+            "#d0d063633d3d"
+            "#505095955252"
+            "#e5e5c6c6aaaa"
+            "#67675f5f5454"
+            "#ffff64645a5a"
+            "#9898e0e03636"
+            "#e0e0d5d56161"
+            "#5f5fdadaffff"
+            "#ffff92926969"
+            "#8484f0f08888"
+            "#f6f6f7f7ecec"
           ];
         };
       };
