69058df08b93dd93116d7c6d47edcee8cc5a1d89
Gabor Pihaj <gabor.pihaj@gmail.com>
2020-01-31 20:19:01 +0000

    Pin haskell ide engine with niv and revert back to the stable channel

--- modules/haskell/default.nix
+++ modules/haskell/default.nix
@@ -1,13 +1,14 @@
 {pkgs, ...}:
 
 let
-  all-hies = import (fetchTarball "https://github.com/infinisil/all-hies/tarball/master") {};
+  sources = import ../../nix/sources.nix;
+  all-hies = (import sources.all-hies {});
 in
 {
   home.packages = [
     pkgs.cabal-install
     pkgs.cabal2nix
     # Install stable HIE for GHC 8.6.5
-    (all-hies.unstable.selection { selector = p: { inherit (p) ghc865; }; })
+    (all-hies.selection { selector = p: { inherit (p) ghc865; }; })
   ];
 }
