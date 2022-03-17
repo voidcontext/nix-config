bb5d45afb7d5418f6838d556d580fbf877e0901d
Gabor Pihaj <gabor.pihaj@gmail.com>
2022-03-17 22:02:57 +0000

    Create tun user and ssh tunel to deneb

--- hosts/electra/configuration.nix
+++ hosts/electra/configuration.nix
@@ -134,6 +134,11 @@
       shell = "${pkgs.git}/bin/git-shell";
       openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDADgf8KaKWIqwJmQPhyLKLwfUplk6RDQ0j/SgcwuHlVj6WRVJJZbFEutnKn5gfZ75M2Wzmsrn7F1W1/CEvmGohE7bLz00ZpM38Hlw/1U2S7ABZ1GwistN42HBMy/jufme0vb4bzFKWH6sXsEnezg1zUPAJlIBA0OxVuKaTQAQOTIEi1ytVrq2wNa9Iiv+Bb6OeK/Vnt8HFOv1H3xmZNtn/N7X35kO5aCwaUlHPpr/7jxQf02fuNhnc0jU6VVygG7uwlfu3j/1lT7DDeIAEYbIeOXRg6Xn+HzDpHdv6FSipSwp499f8tC3TUZDdXT+iSAL9IOZuaujX0qME4bOJZOJuSGPckj9n97gbzoxFEzPsyAFRDgT7MRzQg4QW0fUj3/R9P/DqtxA8F/qfqOQ+Wy2AJ0M+eXrDuZoxZ4F6j4jKaxoUfylYWplILC9kxkk4q0enocOuzxGM6j9rVg9T1wG4/4auKSqENS5QXsvYAsu63RE4WwxAwxuSIymMwA0WhJ6PGgFzlFHluRP8NVlMeCuCZ+0eopH7hqvwZH4m9RmsnadMk0wkZ6ZjsJ0oeFjIxOysiaQbM9lbE0iuoRKRO4E2pfOXt+Nu94r6W8IUVkGYs7PdpsTntnv2pKh8P28/7uE09/U1DfgyYq8BZ+z9bb7GFwpfuZGCXAAvooZDY40b+Q== cardno:000612711012" ];
     };
+
+    tun = {
+      isNormalUser = true;
+      createHome = true;
+    };
   };
 
   home-manager.users.vdx = import ./home-vdx.nix;
@@ -169,6 +174,24 @@
     enable = true;
   };
 
+    systemd.services.tun-deneb = {
+    description = "SSH Tunnel to deneb";
+    after = [ "network.target" ];
+
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig = {
+      Type = "simple";
+      User = "tun";
+
+      ExecStart = ''
+        ${pkgs.openssh}/bin/ssh -i /home/tun/.ssh/id_rsa -R 5522:localhost:22 tun@vdx.hu -p5422 -N
+      '';
+
+      Restart = "on-failure";
+      RestartSec = 3;
+    };
+  };
 
   services.nginx.enable = true;
   services.nginx.recommendedProxySettings = true;
