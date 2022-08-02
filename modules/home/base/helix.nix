cdfddc217e1c52460590ec407eec291383e43efd
Gabor Pihaj <gabor.pihaj@gmail.com>
2022-08-02 19:45:02 +0100

    Update helix config

--- modules/home/base/helix.nix
+++ modules/home/base/helix.nix
@@ -8,6 +8,12 @@
     package = helix.package;
     settings = {
       theme = "gruvbox";
+      # editor.whitespace.render = "all";
+      editor.whitespace.render.space = "all";
+      editor.whitespace.render.tab = "all";
+      editor.whitespace.render.newline = "none";
+      editor.indent-guides.render = true;
+      editor.indent-guides.character = "╎";
     };
     languages = [
       {
