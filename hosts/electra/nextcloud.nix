9621262bf4c9134a0e00153feab997ad6f6247fc
Gabor Pihaj <gabor.pihaj@gmail.com>
2022-12-09 19:39:11 +0000

    Use ssl (self signed cert) to access nextcloud

--- hosts/electra/nextcloud.nix
+++ hosts/electra/nextcloud.nix
@@ -7,6 +7,7 @@
     home = "/Volumes/raid/nextcloud";
     package = pkgs.nextcloud24;
     maxUploadSize = "20G";
+    https = true;
     config = {
       dbtype = "pgsql";
       dbuser = "nextcloud";
@@ -18,8 +19,13 @@
     };
   };
 
+  # Command to generate the certs:
+  # openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj '/CN=nextcloud.vdx.hu/OU=TEST/O=VDX/L=WALSALL/C=UK/' -keyout ./nextcloud-selfsigned.key -out ./nextcloud-selfsigned.crt  # 
   services.nginx.virtualHosts."nextcloud.vdx.hu" = {
     serverAliases = [ "netxcloud.vdx.hu" ];
+    forceSSL = true;
+    sslCertificate = "/opt/secrets/nextcloud/nextcloud-selfsigned.crt";
+    sslCertificateKey = "/opt/secrets/nextcloud/nextcloud-selfsigned.key";
   };
 	
   # ensure that postgres is running *before* running the setup
