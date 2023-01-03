97810b0f5cb063165517800e0297ae330935b086
Gabor Pihaj <gabor.pihaj@gmail.com>
2023-01-03 09:53:00 +0000

    Bump scala metals to 0.11.10

--- modules/home/development/scala/default.nix
+++ modules/home/development/scala/default.nix
@@ -28,8 +28,8 @@ let
     });
 
   metals = metalsBuilder {
-    version = "0.11.9";
-    outputHash = "sha256-CJ34OZOAM0Le9U0KSe0nKINnxA3iUgqUMtS06YnjvVo=";
+    version = "0.11.10";
+    outputHash = "sha256-CNLBDsyiEOmMGA9r8eU+3z75VYps21kHnLpB1LYC7W4=";
   };
 
   metals-reload = pkgs.writeShellScriptBin "metals-reload" ''
