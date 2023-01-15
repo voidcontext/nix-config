0236a468083ec2cfa1b671215d15ef4d44e713b5
Gabor Pihaj <gabor.pihaj@gmail.com>
2023-01-15 15:42:43 +0000

    Make nvidia-offload (optimus) work on albeiro

--- hosts/albeiro/hardware-configuration.nix
+++ hosts/albeiro/hardware-configuration.nix
@@ -8,15 +8,15 @@
   modulesPath,
   ...
 }:
-# let
-#   nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
-#     export __NV_PRIME_RENDER_OFFLOAD=1
-#     export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-#     export __GLX_VENDOR_LIBRARY_NAME=nvidia
-#     export __VK_LAYER_NV_optimus=NVIDIA_only
-#     exec -a "$0" "$@"
-#   '';
-# in
+let
+  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec -a "$0" "$@"
+  '';
+in
 {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
@@ -38,21 +38,21 @@
 
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
-  #  services.xserver.videoDrivers = [ "nvidia" ];
+  services.xserver.videoDrivers = [ "nvidia" ];
   hardware.opengl.enable = true;
-  # hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_390;
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
 
-  # hardware.nvidia.prime = {
-  #   offload.enable = true;
+  hardware.nvidia.prime = {
+    offload.enable = true;
 
-  #   # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
-  #   intelBusId = "PCI:0:2:0";
+    # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
+    intelBusId = "PCI:0:2:0";
 
-  #   # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
-  #   nvidiaBusId = "PCI:1:0:0";
-  # };
+    # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
+    nvidiaBusId = "PCI:1:0:0";
+  };
 
-  # environment.systemPackages = [
-  #   nvidia-offload
-  # ];
+  environment.systemPackages = [
+    nvidia-offload
+  ];
 }
