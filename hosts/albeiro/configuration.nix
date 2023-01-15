6d54b7525e7e6585f2c60b237b620124d0ad78ae
Gabor Pihaj <gabor.pihaj@gmail.com>
2023-01-15 14:09:13 +0000

    Enable ssh agent pam auth on albeiro

--- hosts/albeiro/configuration.nix
+++ hosts/albeiro/configuration.nix
@@ -83,6 +83,10 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
+  security.sudo.enable = true;
+  security.pam.enableSSHAgentAuth = true;
+  security.pam.services.sudo.sshAgentAuth = true;
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.vdx = {
     isNormalUser = true;
