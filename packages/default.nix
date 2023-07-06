613f1d8b06397e04cbce2e311e6e427a64be6fa3
Gabor Pihaj <gabor.pihaj@gmail.com>
2023-07-06 09:48:45 +0100

    refactor: use cocogitto from nixpkgs unstable and remove local package

--- packages/default.nix
+++ packages/default.nix
@@ -1,11 +1,9 @@
 {pkgs, ...}: let
   adr-tools = import ./adr-tools.nix {inherit pkgs;};
-  cocogitto-dev = import ./cocogitto-dev.nix {inherit pkgs;};
   tfswitch = import ./tfswitch.nix {inherit pkgs;};
 in {
   inherit
     adr-tools
-    cocogitto-dev
     tfswitch
     ;
 }
