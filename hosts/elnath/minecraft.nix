efa4037deadf18caa2cd32b42593714e909ef914
Gabor Pihaj <gabor.pihaj@gmail.com>
2023-12-26 22:59:04 +0000

    Finalize minecraft-server config

--- hosts/elnath/minecraft.nix
+++ hosts/elnath/minecraft.nix
@@ -4,9 +4,10 @@
 }: let
   serverPort = 36456;
   bedrockServerPort = 19132;
+  secrets = import ./secrets.nix;
 in {
   services.minecraft-server = {
-    enable = false;
+    enable = true;
     package = pkgs.unstable.minecraft-server.override {
       version = "1.20.4";
       url = "https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar";
@@ -14,29 +15,28 @@ in {
     };
     eula = true;
     declarative = true;
-    whitelist = {
-      voidcontext = "8488f0e8-5594-4b74-84aa-6ac7ceafe64b";
-    };
+    whitelist = secrets.allowList;
     serverProperties = {
       "server-port" = serverPort;
       "white-list" = true;
     };
-    jvmOpts = "-Xms1024M -Xmx1024M -XX:+UseG1GC -XX:ParallelGCThreads=2 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10";
+    jvmOpts = "-Xms512M -Xmx1024M -XX:+UseG1GC -XX:ParallelGCThreads=2 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10";
   };
-  # networking.firewall.allowedUDPPorts = [serverPort];
-  # networking.firewall.allowedTCPPorts = [serverPort];
+
+  systemd.services.minecraft-server.wantedBy = [];
+  
+  networking.firewall.allowedUDPPorts = [serverPort];
+  networking.firewall.allowedTCPPorts = [serverPort];
 
 
-  services.minecraft-bedrock-server = {
-    enable = true;
-    serverProperties = {
-      server-port = bedrockServerPort;
-    };
-    allowList = {
-      voidcontext = "8488f0e8-5594-4b74-84aa-6ac7ceafe64b";
-    };
-  };
-  networking.firewall.allowedUDPPorts = [bedrockServerPort];
-  networking.firewall.allowedTCPPorts = [bedrockServerPort];
+  # services.minecraft-bedrock-server = {
+  #   enable = true;
+  #   serverProperties = {
+  #     server-port = bedrockServerPort;
+  #   };
+  #   allowList = secrets.allowList;
+  # };
+  # networking.firewall.allowedUDPPorts = [bedrockServerPort];
+  # networking.firewall.allowedTCPPorts = [bedrockServerPort];
 
 }
