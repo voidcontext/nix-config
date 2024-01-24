3b3151baf3839890be4fb88a63a444c6fc59470d
Gabor Pihaj <gabor.pihaj@gmail.com>
2024-01-24 21:03:43 +0000

    Update nixpkgs-unstable + use k3s from unstable on kraz

--- hosts/kraz/configuration.nix
+++ hosts/kraz/configuration.nix
@@ -122,7 +122,7 @@
 
   security.acme.defaults.email = "admin+acme@vdx.hu";
   security.acme.acceptTerms = true;
-  services.nginx.enable = true;
+  # services.nginx.enable = true;
   services.nginx.recommendedProxySettings = true;
   networking.firewall.allowedTCPPorts = [
     # nginx
@@ -138,16 +138,44 @@
 
   # k3s
   services.k3s.enable = true;
+  services.k3s.package = pkgs.unstable.k3s_1_29;
   services.k3s.role = "server";
   environment.systemPackages = [pkgs.k3s];
   networking.firewall.interfaces."wg0".allowedTCPPorts = [
     6443
   ];
-  networking.firewall.trustedInterfaces = [ "cni+" ];
+  networking.firewall.trustedInterfaces = ["cni+"];
 
+  services.nginx.virtualHosts."staging.gaborpihaj.com" = {
+    enableACME = true;
+    forceSSL = true;
+    locations."^~ /.well-known/acme-challenge" = {
+      proxyPass = "http://localhost:8080";
+      extraConfig = ''
+        auth_basic off;
+      '';
+    };
+    locations."/" = {
+      proxyPass = "http://localhost:8443";
+      basicAuthFile = "/opt/secrets/nginx/staging.gaborpihaj.com.htpasswd";
+    };
+  };
+  services.nginx.virtualHosts."gaborpihaj.com" = {
+    enableACME = false;
+    forceSSL = true;
+    sslCertificate = "/opt/secrets/nginx/gaborpihaj.com/fullchain.pem";
+    sslCertificateKey = "/opt/secrets/nginx/gaborpihaj.com/key.pem";
+    sslTrustedCertificate = "/opt/secrets/nginx/gaborpihaj.com/chain.pem";
+    locations."/" = {
+      proxyPass = "http://localhost:8443";
+    };
+  };
   services.xinetd.enable = true;
   services.xinetd.services = [
-    { name = "svn"; unlisted = true; port = 32101;
+    {
+      name = "svn";
+      unlisted = true;
+      port = 32101;
       server = "/usr/bin/env"; # not used if "redirect" is specified, but required by Nixos, *and* must be executable
       extraConfig = "redirect = 10.43.16.47 32101";
     }
