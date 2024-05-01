aea59b317f4309a618b3ae893f864adb20906b0f
Gabor Pihaj <gabor.pihaj@gmail.com>
2024-05-01 13:47:11 +0100

    Improve jj wrapper

--- pkgs/jj.nix
+++ pkgs/jj.nix
@@ -1,12 +1,16 @@
 {pkgs, ...}:
 pkgs.writeShellScriptBin "jj" ''
-  if [ -f .__DANGER__ ] && [ "$1" == "git" ] && [ "$2" == "push" ]; then
-    cat << EOF
+  if [ "$1" == "git" ] && [ "$2" == "push" ]; then
+    echo -n "Checking if pushing is safe... "
+    if test -f .__DANGER__ || grep -q '!DANGER!' <<< $(jj log -r ::@); then 
+      cat << EOF
   !!!DANGER!!!
 
   Secrets might be exposed!
   EOF
-    exit 1
+      exit 1
+    fi
+    echo "Done."
   fi
 
   ${pkgs.unstable.jujutsu}/bin/jj "$@"
