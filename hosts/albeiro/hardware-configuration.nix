498d429dcfdabcb92621b2e3f8ae3228624dbfed
Gabor Pihaj <gabor.pihaj@gmail.com>
2024-05-01 11:23:28 +0100

    Fix uconsole wifi after upgrade

--- hosts/albeiro/hardware-configuration.nix
+++ hosts/albeiro/hardware-configuration.nix
@@ -20,6 +20,8 @@ in {
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
+  hardware.enableRedistributableFirmware = true;
+
   boot.initrd.availableKernelModules = ["xhci_pci" "ehci_pci" "ahci" "usb_storage" "sd_mod" "sr_mod" "rtsx_pci_sdmmc"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-intel"];
