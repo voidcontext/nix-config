03158e38774428e0fcc6ce0629f7440720e2e84b
Gabor Pihaj <gabor.pihaj@gmail.com>
2024-05-14 10:25:10 +0100

    Bump Scala Metals to 1.3.1

--- modules/home/development/scala/default.nix
+++ modules/home/development/scala/default.nix
@@ -33,8 +33,8 @@ with lib; let
     });
 
   metals = metalsBuilder {
-    version = "1.3.0";
-    outputHash = "sha256-otN4sqV2a0itLOoJ7x+VSMe0tl3y4WVovbA1HOpZVDw=";
+    version = "1.3.1";
+    outputHash = "sha256-ugTYjXgD5SHagRtc1RNsnfcLAXPeWSHcos82ewr3UIs=";
   };
 
   metals-reload = pkgs.writeShellScriptBin "metals-reload" ''
