700a6ec16e5187351d2f6be9b0fe81e24a2d8d52
Gabor Pihaj <gabor.pihaj@gmail.com>
2024-10-24 14:53:52 +0100

    Bump Scala Metals to 1.4.0

--- defaults.nix
+++ defaults.nix
@@ -71,8 +71,8 @@
   };
   scalaMetalsOverlay = final: prev: let
     # From: https://github.com/gvolpe/neovim-flake/blob/main/lib/metalsBuilder.nix
-    version = "1.3.5";
-    outputHash = "sha256-86/zeoOO5kSAwh7uQTV7nGUGQoIux1rlH5eUgvn3kvY=";
+    version = "1.4.0";
+    outputHash = "sha256-mmsCdv3zSwsaA00I5sQVy0V4fl1GytdgjVjs2r6x32Q=";
     metalsDeps = final.stdenv.mkDerivation {
       name = "metals-deps-${version}";
       buildCommand = ''
